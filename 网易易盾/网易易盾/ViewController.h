d8b4b9c67db4ca651d94e231bae616ec30582ae8
BG <fubinggui@aspirecn.com>
2017-07-13 10:17:25 +0800

    创建文件

---
+++ 网易易盾/网易易盾/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  网易易盾
+//
+//  Created by 丙贵 on 17/7/13.
+//  Copyright © 2017年 Jenocy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 网易易盾/网易易盾/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  网易易盾
+//
+//  Created by 丙贵 on 17/7/13.
+//  Copyright © 2017年 Jenocy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
